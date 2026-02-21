31a01bac4e5810cf197ada3f8cb225671b8fb2a4
Travis-DV <findleytm2018@gmail.com>
2026-02-20 20:34:13 -0600

    Added base card in html and actual one in js.

--- PopulateT3.js
+++ PopulateT3.js
@@ -9,7 +9,18 @@ const number = urlParams.get('number');
 const dictionary = {
     "restaurant": {
         "navId": "tasteNav",
-        "breadcrumbName": "Restaurants"
+        "breadcrumbName": "Restaurants",
+        "items": [
+            {
+                "imageSrc": "restaurant1",
+                "imageAlt": "Two hamburgers next to each other on a cutting board.",
+                "title": "Restaurant 1",
+                "stars": 5, //out of 5
+                "money": 3, //out of 3
+                "description": "The most popular restaurant on the island"
+
+            }
+        ]
     },
     "grocery": {},
     "ent": {},
@@ -46,3 +57,101 @@ breadcrumbLinkElement.textContent = dictionary[type]["breadcrumbName"];
 breadcrumbListElement.append(breadcrumbLinkElement);
 let breadcrumb = document.getElementById("breadcrumbList");
 breadcrumb.append(breadcrumbListElement);
+
+const items = dictionary[type]["items"];
+const main = document.getElementById('main');
+
+for (let i = 0; i < items.length; i++) {
+    let cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    cardDiv.classList.add("t3card");
+    cardDiv.classList.add("col-md")
+    main.append(cardDiv);
+
+    let cardImg = document.createElement("img");
+    cardImg.classList.add("card-img-top");
+    cardImg.src = "images/" + items[i]["imageSrc"] + ".jpg";
+    cardImg.alt = items[i]["imageAlt"];
+    cardDiv.append(cardImg);
+
+    let overlayDiv = document.createElement("div");
+    overlayDiv.classList.add("card-img-overlay");
+    overlayDiv.classList.add("p-0");
+    overlayDiv.classList.add("d-flex");
+    overlayDiv.classList.add("flex-column");
+    cardDiv.append(overlayDiv);
+
+    let cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title");
+    cardTitle.classList.add("t3CardTitle");
+    cardTitle.classList.add("w-auto");
+    cardTitle.classList.add("p-1");
+    cardTitle.textContent = items[i]["title"];
+    overlayDiv.append(cardTitle);
+
+    let iconDiv = document.createElement("div");
+    iconDiv.classList.add("mt-auto");
+    iconDiv.classList.add("p-1");
+    iconDiv.classList.add("d-flex");
+    iconDiv.classList.add("flex-row");
+    iconDiv.classList.add("iconDiv")
+    overlayDiv.append(iconDiv);
+
+    let starDiv = document.createElement("div");
+    starDiv.classList.add("d-flex");
+    starDiv.classList.add("justify-content-center");
+    iconDiv.append(starDiv);
+
+    for (let s = 0; s < 5; s++) {
+        let starImg = document.createElement("img");
+        starImg.classList.add("mt-auto");
+
+        if (s <= (items[i]["stars"]-1)) {
+            starImg.src = "/images/fullStar.svg";
+            starImg.alt = "A star icon that is filled in.";
+            starImg.style.maxHeight = "1rem";
+        }
+        else {
+            starImg.src = "/images/emptyStar.svg";
+            starImg.alt = "A star icon that is not filled in.";
+            starImg.style.maxHeight = "1rem";
+        }
+
+        starDiv.append(starImg);
+    }
+
+    let moneyDiv = document.createElement("div");
+    moneyDiv.classList.add("ms-auto");
+    moneyDiv.classList.add("d-flex");
+    moneyDiv.classList.add("justify-content-center");
+    iconDiv.append(moneyDiv);
+
+    for (let m = 0; m < items[i]["money"]; m++) {
+        let moneyImg = document.createElement("img");
+        moneyImg.classList.add("mt-auto");
+        moneyImg.src = "/images/money.svg";
+        moneyImg.alt = "A star icon that is filled in.";
+        moneyImg.style.maxHeight = "1rem";
+        moneyDiv.append(moneyImg);
+    }
+
+    let cardBodyDiv = document.createElement("div");
+    cardBodyDiv.classList.add("card-body");
+    cardDiv.append(cardBodyDiv);
+
+    let cardTextP = document.createElement("p");
+    cardTextP.classList.add("card-text");
+    cardTextP.textContent = items[i]["description"];
+    cardBodyDiv.append(cardTextP);
+
+    cardImg.onload = function() {
+
+        const imgHeight = cardImg.clientHeight;
+        const imgWidth = cardImg.clientWidth;
+
+        overlayDiv.style.height = `${imgHeight}px`;
+
+        starDiv.style.width = `${(imgWidth*0.045)*items[i]["stars"]*1.25}px`
+        moneyDiv.style.width = `${(imgWidth*0.05)*1.875}px`
+    };
+}
